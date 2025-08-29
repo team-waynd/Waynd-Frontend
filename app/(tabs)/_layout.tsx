afa2f2c027cde30f0d1d7e5487d62034b53bbd12
minwoo1119 <minwoo1119@naver.com>
2025-08-29 10:33:20 +0900

    [Feat] #1 - 기본 파일 구성 정리

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,50 +1,13 @@
-import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
-import React from 'react';
 
-export default function TabLayout() {
+export default function TabsLayout() {
     return (
-        <Tabs
-            screenOptions={{
-                headerShown: false,
-                tabBarStyle: {
-                    backgroundColor: '#AAD2FF',
-                    paddingTop: 20,
-                    height: 80,
-                    position: 'absolute',
-                },
-                tabBarInactiveTintColor: '#FAFAF8',
-                tabBarActiveTintColor: '#4B90E2',
-            }}
-        >
-            <Tabs.Screen
-                name="index"
-                options={{
-                    title: '',
-                    tabBarIcon: ({ color }) => <Ionicons size={28} name="golf" color={color} />,
-                }}
-            />
-            <Tabs.Screen
-                name="festival"
-                options={{
-                    title: '',
-                    tabBarIcon: ({ color }) => <Ionicons size={28} name="star" color={color} />,
-                }}
-            />
-            <Tabs.Screen
-                name="community"
-                options={{
-                    title: '',
-                    tabBarIcon: ({ color }) => <Ionicons size={28} name="people" color={color} />,
-                }}
-            />
-            <Tabs.Screen
-                name="mypage"
-                options={{
-                    title: '',
-                    tabBarIcon: ({ color }) => <Ionicons size={28} name="person" color={color} />,
-                }}
-            />
+        <Tabs>
+            <Tabs.Screen name="user" options={{ title: 'User' }} />
+            <Tabs.Screen name="place" options={{ title: 'Place' }} />
+            <Tabs.Screen name="festival" options={{ title: 'Festival' }} />
+            <Tabs.Screen name="post" options={{ title: 'Post' }} />
+            <Tabs.Screen name="review" options={{ title: 'Review' }} />
         </Tabs>
     );
 }
---
+++ app/(tabs)/mypage/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function MyPage() {
+    return (
+        <SafeAreaView>
+            <Text>This is MyPage</Text>
+        </SafeAreaView>
+    );
+}
